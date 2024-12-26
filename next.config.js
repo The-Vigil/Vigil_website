668157de488c6ad5258efa2426a47d4cbb1d6613
Shahrukh Masood Siddiqui <32538996+shahrukhsiddiqui@users.noreply.github.com>
2024-12-27 04:46:35 +0530

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    env: {
+      NEXT_PUBLIC_RUNPOD_API_KEY: process.env.NEXT_PUBLIC_RUNPOD_API_KEY,
+      NEXT_PUBLIC_ENDPOINT_ID: process.env.NEXT_PUBLIC_ENDPOINT_ID,
+    },
+  };
+  
